5e289723ec0f8c11fc0ffe4320abe1c5c9393759
Aleksey <11_alx_11@ro.ru>
2023-05-03 07:48:42 +0300

    prep parallel work

--- main/main.cpp
+++ main/main.cpp
@@ -17,17 +17,21 @@ int main()
 	BMPio* bmp_io = new BMPio();
 
 	// Init variables.
-	// std::string filename = "Garfield.bmp";
+	std::string filename = "Garfield.bmp";
 	// std::string filename = "BDL5000_monochrome.bmp";
-	std::string filename = "test64_mono.bmp";
+	// std::string filename = "test64_mono.bmp";
 	bool* pixels = nullptr;
 	int pixels_height;
 	int pixels_width;
 
+	// Consistently
+	// ***********************************************************************************
+	cout << "Consistent:\n\n";
+
 	// Init timer.
 	auto timer = std::chrono::steady_clock::now();
 
-	// ToDo reading memory (supposed) issue.
+	// ToDo reading memory issue.
 	if (bmp_io->read(filename, pixels, pixels_height, pixels_width)) {
 		cerr << "Error reading image!" << endl;
 		return 1;
@@ -44,6 +48,7 @@ int main()
 
 	// Start timer.
 	timer = std::chrono::steady_clock::now();
+	auto cons_timer = timer;
 
 	// Apply median filter.
 	if (filters->medianFilter(pixels, pixels_new1, pixels_height, pixels_width)) {
@@ -68,6 +73,9 @@ int main()
 	time = std::chrono::steady_clock::now() - timer;
 	cout << "Filtering (rotate180deg) bmp: " << time.count() << "s" << endl;
 
+	std::chrono::duration<double> cons_time = std::chrono::steady_clock::now() - cons_timer;
+	cout << "Consistent filtering bmp: " << cons_time.count() << "s" << endl;
+
 	// Start timer.
 	timer = std::chrono::steady_clock::now();
 
@@ -88,15 +96,80 @@ int main()
 	time = std::chrono::steady_clock::now() - timer;
 	cout << "Saving bmps: " << time.count() << "s" << endl;
 
-	/* Testing
-	for (int i = 0; i < pixels_height; i++) {
-		cout << i << " :";
-		for (int j = 0; j < pixels_width; j++) {
-			cout << ((pixels[i * pixels_width + j]) ? 'w' : 'b');
-		}
-		cout << endl;
+	// Parallel
+	// ******************************************************************************************
+	cout << "\nParallel\n\n";
+
+	pixels = nullptr;
+
+	// Init timer.
+	timer = std::chrono::steady_clock::now();
+
+	// ToDo reading memory issue.
+	if (bmp_io->read(filename, pixels, pixels_height, pixels_width)) {
+		cerr << "Error reading image!" << endl;
+		return 1;
 	}
 
-	// */
+	// Timer end and out.
+	time = std::chrono::steady_clock::now() - timer;
+	cout << "Reading bmp: " << time.count() << "s" << endl;
+
+	pixels_new1 = nullptr;
+	pixels_new2 = nullptr;
+
+	// Start timer.
+	timer = std::chrono::steady_clock::now();
+	auto paral_timer = timer;
+
+	// Apply median filter.
+	if (filters->medianFilter(pixels, pixels_new1, pixels_height, pixels_width)) {
+		cerr << "Error filtering(median) image!" << endl;
+		return 3;
+	}
+
+	// Timer end and out.
+	time = std::chrono::steady_clock::now() - timer;
+	cout << "Filtering (median) bmp: " << time.count() << "s" << endl;
+
+	// Start timer.
+	timer = std::chrono::steady_clock::now();
+
+	// Apply rotation filter.
+	if (filters->rotate180degFilter(pixels_new1, pixels_new2, pixels_height, pixels_width)) {
+		cerr << "Error filtering(rotate180deg) image!" << endl;
+		return 3;
+	}
+
+	// Timer end and out.
+	time = std::chrono::steady_clock::now() - timer;
+	cout << "Filtering (rotate180deg) bmp: " << time.count() << "s" << endl;
+
+	std::chrono::duration<double> paral_time = std::chrono::steady_clock::now() - paral_timer;
+	cout << "Parallel filtering bmp: " << paral_time.count() << "s" << endl;
+
+	// Start timer.
+	timer = std::chrono::steady_clock::now();
+
+	// Save results.
+	if (bmp_io->writeWithHeader("test1_out.bmp", pixels, pixels_height, pixels_width)) {
+		cerr << "Error saving image!" << endl;
+		return 2;
+	}
+	if (bmp_io->writeWithHeader("test2_out.bmp", pixels_new1, pixels_height, pixels_width)) {
+		cerr << "Error saving image!" << endl;
+		return 2;
+	}
+	if (bmp_io->writeWithHeader("test3_out.bmp", pixels_new2, pixels_height, pixels_width)) {
+		cerr << "Error saving image!" << endl;
+		return 2;
+	}
+	// Timer end and out.
+	time = std::chrono::steady_clock::now() - timer;
+	cout << "Saving bmps: " << time.count() << "s" << endl;
+
+	// End, main section again
+
+	cout << "\nEffectiveness: " << cons_time / paral_time << endl;
 	return 0;
 }
